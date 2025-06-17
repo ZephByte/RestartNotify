54fd9173685e83a2b3ab5bea54fbcc0ac2ec81a1
ZephByte <cloudedsprite@gmail.com>
2025-06-17 18:49:07 -0500

    Really gross, really really gross. But should work for now. MVP lets go.

--- src/main/kotlin/com/zephbyte/restartnotify/Countdown.kt
+++ src/main/kotlin/com/zephbyte/restartnotify/Countdown.kt
@@ -7,16 +7,16 @@ import net.minecraft.util.Formatting
 
 class Countdown(private val context: CommandContext<ServerCommandSource>) { // context is an instance property
 
-    fun initiateCountdown() {
-        broadcastMessage() // Uses the instance's context
-    }
+//    fun initiateCountdown() {
+//        broadcastMessage() // Uses the instance's context
+//    }
 
-    private fun broadcastMessage() {
+    fun broadcastMessage(timeLeft: Int) {
         // 'context' is available directly from the class instance
         context.source.server.playerManager.playerList.forEach { player ->
             player.sendMessage(
-                Text.literal("Daily restart in 15 minutes!").formatted(
-                    Formatting.DARK_PURPLE
+                Text.literal("Daily restart in $timeLeft minute${if (timeLeft > 1) "s" else ""}!").formatted(
+                    Formatting.LIGHT_PURPLE, Formatting.BOLD
                 ), true
             )
         }
--- src/main/kotlin/com/zephbyte/restartnotify/ModCommands.kt
+++ src/main/kotlin/com/zephbyte/restartnotify/ModCommands.kt
@@ -9,24 +9,46 @@ object ModCommands {
 
     fun register() {
         CommandRegistrationCallback.EVENT.register { dispatcher, registryAccess, environment ->
-            registerInitiateRestartNotify(dispatcher)
+            //registerInitiateRestartNotify(dispatcher)
+            registerTimeCommands(dispatcher)
         }
     }
 
-    private fun registerInitiateRestartNotify(
+//    private fun registerInitiateRestartNotify(
+//        dispatcher: CommandDispatcher<ServerCommandSource>
+//    ) {
+//        dispatcher.register(
+//            CommandManager.literal(MOD_ID)
+//                .then(
+//                    CommandManager.literal("start")
+//                        .requires { source -> source.hasPermissionLevel(2) }
+//                        .executes { context ->
+//                            val countdown = Countdown(context)
+//                            countdown.initiateCountdown()
+//                            1
+//                        }
+//                )
+//        )
+//    }
+
+    private fun registerTimeCommands(
         dispatcher: CommandDispatcher<ServerCommandSource>
     ) {
-        dispatcher.register(
-            CommandManager.literal(MOD_ID)
-                .then(
-                    CommandManager.literal("start")
-                        .requires { source -> source.hasPermissionLevel(2) }
-                        .executes { context ->
-                            val countdown = Countdown(context)
-                            countdown.initiateCountdown()
-                            1
-                        }
-                )
-        )
+        val warningTimes = intArrayOf(20, 15, 10, 5, 1)
+
+        for(time in warningTimes) {
+            dispatcher.register(
+                CommandManager.literal(MOD_ID)
+                    .then(
+                        CommandManager.literal(time.toString())
+                            .requires { source -> source.hasPermissionLevel(2) }
+                            .executes { context ->
+                                val countdown = Countdown(context)
+                                countdown.broadcastMessage(time)
+                                1
+                            }
+                    )
+            )
+        }
     }
 }
